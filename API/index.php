3706c95cf86465955ab6ef7de5d03edfb45096c5
Dakota Knutson <dakotak@outlook.com>
2014-09-29 11:47:26 -0700

    Added Slim framework.

--- API/index.php
+++ API/index.php
@@ -1,3 +1,16 @@
 <?php
 // index.php
 
+// Composer autoloader
+reqire 'vendor/autoload.php';
+
+$app = new \Slim\Slim(array(
+	'mode' => 'development',
+	'debug' => true
+	));
+
+
+// Define Routes
+
+
+$app->run();
